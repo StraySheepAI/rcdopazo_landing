689b45cd52a78c4b5bbbd62cbdbd75b66e713fb2
RC Dopazo <mpaflow.ai@gmail.com>
2026-09-01 11:40:30 -0300

    Reservar el Mundo Dima y conectar su dominio

--- next.config.ts
+++ next.config.ts
@@ -29,6 +29,16 @@ const nextConfig: NextConfig = {
           ],
           destination: "/stray-sheep",
         },
+        {
+          source: "/",
+          has: [
+            {
+              type: "host",
+              value: "dima.magiaparaatrevidos.com",
+            },
+          ],
+          destination: "/dima",
+        },
       ],
       afterFiles: [],
       fallback: [],
